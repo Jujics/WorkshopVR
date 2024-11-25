f0b367c6d52b72651e73d4d7ae17e378be4997a0
Philia's Snake <snakephilias@gmail.com>
2024-11-25 12:20:34 +0100

    supression des éléments ne servant à rien sur le menu manager

--- Assets/Scenes/TestUI/MenuManager.cs
+++ Assets/Scenes/TestUI/MenuManager.cs
@@ -5,55 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public static MenuManager Instance { get; private set; }
-    public static GameObject menuMenu, quitMenu;
-
-    private void Awake()
-    {
-        if(Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public static void Init()
-    {
-        GameObject canvas = GameObject.Find("Canvas");
-        menuMenu = canvas.transform.Find("MenuMenu").gameObject;
-        quitMenu = canvas.transform.Find("QuitMenu").gameObject;
-    }
-
-    public static void OpenMenu(Menu menu, GameObject callingMenu)
-    {
-        if (!menuMenu || !quitMenu)
-            Init();
-
-        switch (menu)
-        {
-            case Menu.MENU:
-                menuMenu.SetActive(true);
-                MenuPlay();
-                break;
-                
-            case Menu.QUIT:
-                quitMenu.SetActive(true);
-                MenuQuit();
-                break;
-        }
-
-        callingMenu.SetActive(false);
-    }
-
-    private static void MenuPlay()
-    {
-        throw new NotImplementedException();
-    }
-
     public static void MenuPlay(string sceneName)
     {
         Debug.Log("sceneName to Load: " + sceneName);
