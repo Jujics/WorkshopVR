b29e26e0cc9b173392caa0d7031bd937ede686d9
Philia's Snake <snakephilias@gmail.com>
2024-11-26 10:01:02 +0100

    Mise en place d'un ui 3d pour le menu du jeu

--- Assets/Scenes/TestUI/MenuManager.cs
+++ Assets/Scenes/TestUI/MenuManager.cs
@@ -1,16 +1,57 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public static void MenuPlay(string sceneName)
+    [SerializeField] private GameObject CanvasQuit;
+    [SerializeField] private GameObject CanvasPlay;
+    [SerializeField] private GameObject LumiereH;
+    [SerializeField] private GameObject LumiereB;
+
+    public void Start()
     {
-        Debug.Log("sceneName to Load: " + sceneName);
-        SceneManager.LoadScene(sceneName);
+        CanvasQuit.SetActive(false);
+        CanvasPlay.SetActive(false);
+        LumiereH.SetActive(false);
+        LumiereB.SetActive(false);
     }
 
-    public static void MenuQuit()
+    public void MenuPlay()
     {
+        StartCoroutine(ButtonPlay());
+    }
+
+    public void MenuQuit()
+    {
+        StartCoroutine(ButtonQuit());
+    }
+
+    public IEnumerator ButtonPlay()
+    {
+        yield return new WaitForSeconds(0);
+        CanvasPlay.SetActive(true);
+        LumiereH.SetActive(true);
+        LumiereB.SetActive(true);
+        yield return new WaitForSeconds(2);
+        CanvasPlay.SetActive(false);
+        LumiereH.SetActive(false);
+        LumiereB.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public IEnumerator ButtonQuit()
+    {
+        yield return new WaitForSeconds(0);
+        CanvasQuit.SetActive(true);
+        LumiereH.SetActive(true);
+        LumiereB.SetActive(true);
+        yield return new WaitForSeconds(2);
+        CanvasQuit.SetActive(false);
+        LumiereH.SetActive(false);
+        LumiereB.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
         Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;
     }
