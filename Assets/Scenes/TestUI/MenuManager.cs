d6114906c5ef0ce1d03e02839006bf9f3a36cf2a
Philia's Snake <snakephilias@gmail.com>
2024-11-28 12:23:27 +0100

    MenuMain Let's go

--- Assets/Scenes/TestUI/MenuManager.cs
+++ Assets/Scenes/TestUI/MenuManager.cs
@@ -1,23 +1,13 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject CanvasQuit;
-    [SerializeField] private GameObject CanvasPlay;
-    [SerializeField] private GameObject LumiereH;
-    [SerializeField] private GameObject LumiereB;
+    [SerializeField] private TextMeshProUGUI quitText;
+    [SerializeField] private TextMeshProUGUI playText;
     [SerializeField] private string sceneName;
-
-    public void Start()
-    {
-        CanvasQuit.SetActive(false);
-        CanvasPlay.SetActive(false);
-        LumiereH.SetActive(false);
-        LumiereB.SetActive(false);
-    }
 
     public void MenuPlay()
     {
@@ -32,29 +22,17 @@ public class MenuManager : MonoBehaviour
     public IEnumerator ButtonPlay()
     {
         yield return new WaitForSeconds(0);
-        CanvasPlay.SetActive(true);
-        LumiereH.SetActive(true);
-        LumiereB.SetActive(true);
+        playText.color = Color.blue;
         yield return new WaitForSeconds(2);
-        CanvasPlay.SetActive(false);
-        LumiereH.SetActive(false);
-        LumiereB.SetActive(false);
-        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneName);
     }
 
     public IEnumerator ButtonQuit()
     {
         yield return new WaitForSeconds(0);
-        CanvasQuit.SetActive(true);
-        LumiereH.SetActive(true);
-        LumiereB.SetActive(true);
+        quitText.color = Color.red;
         yield return new WaitForSeconds(2);
-        CanvasQuit.SetActive(false);
-        LumiereH.SetActive(false);
-        LumiereB.SetActive(false);
-        yield return new WaitForSeconds(0.5f);
-        //Application.Quit();
-        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
